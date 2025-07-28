6a553ba4c15893e246fc0e6637cde21035f1b48f
Freeze-ssp <shashwatssp@gmail.com>
2025-07-28 22:42:40 +0530

    componennts

--- src/components/DreamForm.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DreamForm = () => {
-  return (
-    <div>DreamForm</div>
-  )
-}
-
-export default DreamForm
--- src/components/OutputPreview.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OutputPreview = () => {
-  return (
-    <div>OutputPreview</div>
-  )
-}
-
-export default OutputPreview
